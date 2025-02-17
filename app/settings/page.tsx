02fe34d77c263dc22bc6858bd94394428e5fddc9
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-02-18 01:13:34 +0530

    fix: add theme compability

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
-    <div className="">
-      <h2>Settings</h2>
+    <div className="w-full h-screen bg-gray-200 dark:bg-neutral-950">
+      <h2 className="dark:text-gray-300">Settings</h2>
     </div>
   );
 }
